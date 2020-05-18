ba3e3d000c53d9ce5cf2f2e9ea9385669f7825ba
kenesovb <kenesovbt@gmail.com>
2020-05-19 01:01:42 +0600

    secret ket changes

--- tourism/settings.py
+++ tourism/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['tourismkz.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['tourismera.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
