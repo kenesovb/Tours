be0c37c8aea0879c3e12922c356fc734a2f6baf6
kenesovb <kenesovbt@gmail.com>
2020-05-25 15:29:12 +0600

    changed media storage

--- tourism/settings.py
+++ tourism/settings.py
@@ -22,9 +22,9 @@ TEMPLATE_DIRS = (( BASE_DIR +'/templates/' ),)
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zo0ycy@9oyt&$2z)k7w%7=l4nefe@t^jpwh!72(q2d=8ve7=7a'
+# SECRET_KEY = 'zo0ycy@9oyt&$2z)k7w%7=l4nefe@t^jpwh!72(q2d=8ve7=7a'
 # production comment
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # when prod debug false, allowed_hosts = [tourismera]
