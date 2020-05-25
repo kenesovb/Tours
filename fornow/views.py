dd0e45ff45c8722235e3cea136534f0904347e11
kenesovb <kenesovbt@gmail.com>
2020-05-25 22:06:26 +0600

    send email information

--- fornow/views.py
+++ fornow/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib.auth.models import User
 from django.http import Http404
 from django.shortcuts import get_object_or_404
@@ -6,33 +8,33 @@ from rest_framework import permissions
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.core.mail import send_mail, EmailMessage
 from io import BytesIO
 from django.http import HttpResponse
 from xhtml2pdf import pisa
-
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 from .models import Tour, Booking, TourImage, Hotels, TourDetails
-from .serializers import (TourSerializers, TourPageSerializers, UserPageSerializers, TourReviewsPostSerializers, BookingPostSerializers, HotelSerializers,
-                          TourDetailsPostSerializers, BookingSerializers, HotelPageSerializers)
+from .serializers import (TourSerializers, TourPageSerializers, UserPageSerializers, TourReviewsPostSerializers,
+                          BookingPostSerializers, HotelSerializers,
+                          TourDetailsPostSerializers, BookingSerializers, HotelPageSerializers, TourImageSerializer)
 
 
 # Create your views here.
 
 class TourView(APIView):
     """Tour"""
-    permission_classes = [permissions.AllowAny,]
+    permission_classes = [permissions.AllowAny, ]
 
     def get(self, request):
         tours = Tour.objects.all()
         serializer = TourSerializers(tours, many=True)
-        return Response(serializer.data) 
+        return Response(serializer.data)
 
 
 class TourPage(APIView):
-
     """ Page of Tour """
-    
+
     def get(self, request, pk, format=None):
         tour = get_object_or_404(Tour, pk=pk)
         serializer = TourPageSerializers(tour)
@@ -41,7 +43,7 @@ class TourPage(APIView):
 
 class PagesReview(APIView):
     """ Comments for a tour """
-    permission_classes = [permissions.IsAuthenticated,]
+    permission_classes = [permissions.IsAuthenticated, ]
 
     def post(self, request, pk, format=None):
         serializer = TourReviewsPostSerializers(data=request.data)
@@ -54,8 +56,8 @@ class PagesReview(APIView):
 
 class UserDetail(APIView):
     """ User page """
-    permission_classes = [permissions.IsAuthenticated,]
-    
+    permission_classes = [permissions.IsAuthenticated, ]
+
     def get(self, request, format=None):
         user = get_object_or_404(User, username=self.request.user)
         serializer = UserPageSerializers(user)
@@ -65,6 +67,7 @@ class UserDetail(APIView):
 class UserDetailDeleteBooking(APIView):
     """ User page delete tour and create PDF"""
     permission_classes = [permissions.IsAuthenticated, ]
+
     def delete(self, request, pk, format=None):
         book = get_object_or_404(Booking, pk=pk)
         book.delete()
@@ -93,11 +96,9 @@ class UserDetailPDFView(APIView):
         }
         pdf = render_to_pdf('pdf_template.html', paragraph)
         if pdf:
-           response = HttpResponse(pdf, content_type='application/pdf')
-        # ms = EmailMessage('cofirmation', "Everything is ok", 'kenesovbt@gmail.com', [request.user.email])
-        # ms.attach(pdf)
-        # ms.send()
+            response = HttpResponse(pdf, content_type='application/pdf')
         return response
+
 
 class UserDetailPDFViewDownload(APIView):
     """PDF of tour ticket """
@@ -126,7 +127,6 @@ class UserDetailPDFViewDownload(APIView):
         return response
 
 
-
 def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     html = template.render(context_dict)
@@ -138,7 +138,7 @@ def render_to_pdf(template_src, context_dict={}):
 
 class TourBooking(APIView):
     """ Booking of a tour """
-    permission_classes = [permissions.IsAuthenticated,]
+    permission_classes = [permissions.IsAuthenticated, ]
 
     def get(self, request, pk, detailid, format=None):
         tours = get_object_or_404(Tour, pk=pk)
@@ -161,20 +161,47 @@ class BookingPayView(APIView):
     """all Bookings payment update status to approved """
 
     def post(self, request):
+        user = get_object_or_404(User, username=self.request.user)
         bookings = get_object_or_404(Booking, pk=request.data['id'])
         tour = get_object_or_404(Tour, pk=bookings.tour_detail.tour.id)
-        tour.tour_rating = tour.tour_rating+int(bookings.booking_number_of_persons)
+        image = TourImage.objects.filter(tour=bookings.tour_detail.tour.id)[0]
+        tour.tour_rating = tour.tour_rating + int(bookings.booking_number_of_persons)
+        message = Mail(
+            from_email='kenesovbt@gmail.com',
+            to_emails=user.email,
+            subject='Information about your' + bookings.tour_detail.tour.title + 'tour',
+            html_content='<h2>Hello, dear Customer! </h2> <br> <h3>Below some information about your tour </h3> <br>' +
+                         '<strong>' + bookings.tour_detail.tour.title + '</strong>' +
+                         '<p> Starting date: <strong>' + str(bookings.tour_detail.tour_start_date) + '</strong> </p>' +
+                         '<p> End date: <strong>' + str(bookings.tour_detail.tour_end_date) + '</strong> </p>' +
+                         '<p> Starting city: <strong>' + str(
+                bookings.tour_detail.tour.city.city_name) + '</strong> </p>' +
+                         '<p> Ending city: <strong>' + str(bookings.tour_detail.tour.tour_end_city) + '</strong> </p>' +
+                         '<p> Price: <strong>' + str(bookings.tour_detail.tour.price) + ' ' + str(bookings.tour_detail.tour.currency) + '</strong> </p>' +
+                         '<p> Status: <strong>' + str(bookings.booking_status) + '</strong> </p>' +
+                         '<p>you can download your ticket <a href="https://tourismera.herokuapp.com/api/v1/fornow/users/me/' + str(bookings.id) + '/pdf/download"> here </a> /p> <br><br><br><br>' +
+                         '<img src="' + image.file.url + '" width="450px" height="450px">' +
+                         '<h3>Have fun, hope you will enjoy your vacation across Kazakhstan</h3>' +
+                         '<h3>best regards from travel-kazakhstan team.</h3>'
+        )
+        try:
+            sg = SendGridAPIClient(os.environ.get('SendGridAPIClient'))
+            response = sg.send(message)
+        except Exception as e:
+            print(e)
         tour.save()
         bookings.tour_detail.cur_person_number = bookings.tour_detail.cur_person_number + \
                                                  int(bookings.booking_number_of_persons)
         bookings.booking_status = 'Approved'
         bookings.tour_detail.save()
         bookings.save()
+
         return Response(status=status.HTTP_201_CREATED)
 
 
 class HotelsView(APIView):
     """"""
+
     def get(self, request):
         hotels = Hotels.objects.all()
         serializer = HotelSerializers(hotels, many=True)
@@ -183,6 +210,7 @@ class HotelsView(APIView):
 
 class HotelPageView(APIView):
     """"""
+
     def get(self, request, pk):
         hotels = get_object_or_404(Hotels, pk=pk)
         serializer = HotelPageSerializers(hotels)
--- tourism/settings.py
+++ tourism/settings.py
@@ -161,6 +161,7 @@ AWS_LOCATION = 'static'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
 AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
      'CacheControl': 'max-age=86400',
@@ -178,13 +179,11 @@ STATICFILES_FINDERS = (           'django.contrib.staticfiles.finders.FileSystem
 )
 AWS_DEFAULT_ACL = None
 
-
-# SECRET_KEY = config('SECRET_KEY')
 # DEBUG = config('DEBUG', default=False, cast=bool)
 
+# prod setting uncomment
 DATABASES = {
     'default': dj_database_url.config(
-        # default=config('DATABASE_URL')
         default= os.environ.get('DATABASE_URL')
     )
 }
